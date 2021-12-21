18f08c51d156e20373aa62d1c0bed904aab17abd
Dawid <62386190+dawach@users.noreply.github.com>
2021-12-21 17:05:03 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIlna_własne_cyfry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba, silnia = 1;       
+            Console.WriteLine("podaj cyfre z jakiej chcesz obliczyć silnie n!");
+            liczba = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= liczba; i++)
+
+                silnia *= i;
+
+            Console.WriteLine("twój wynik to:");
+            Console.WriteLine(silnia);
+
+
+            Console.ReadKey();
+        }
+    }
+    
+    
+}
