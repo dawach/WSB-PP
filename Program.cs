fb6a8dad248bc69ce8f98f903694fe8c5961b18c
Dawid <62386190+dawach@users.noreply.github.com>
2021-12-21 17:39:01 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator_b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz liczbę pierwszą");
+            int liczbaA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wpisz liczbę drugą");
+            //DAWID ACHREM 2021
+            int liczbaB = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("kalkulator obsługuje dodawanie, odejmowanie i mnożenie");
+            Console.WriteLine("Jeśli chcesz dodać liczby wpisz +, jeśli chcesz odejmować wpisz -, jeśli chcesz mnożyć użyj gwiazdki *");
+            char symbol = Console.ReadLine()[0];
+
+
+            switch (symbol)
+            {
+                case '+':
+
+                    Console.WriteLine("twój wynik dodawania to:");
+                    Console.WriteLine(liczbaA + liczbaB);
+                    break;
+
+                case '-':
+                    Console.WriteLine("twój wynik odejmowania to:");
+                    Console.WriteLine(liczbaA - liczbaB);
+                    break;
+
+                case '*':
+                    Console.WriteLine("twój wynik mnożenia to:");
+                    Console.WriteLine(liczbaA * liczbaB);
+                    break;
+
+                    
+
+                    
+            }
+            Console.ReadKey();
+        }
+    }
+}
+    
+
