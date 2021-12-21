0d9479b640de71ac9aa41ecda733bdb5e6f766e2
Dawid <62386190+dawach@users.noreply.github.com>
2021-12-21 18:10:08 +0100

    Zad--33

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DAWID ACHREM 2021
+            Console.WriteLine("Wpisz liczbę pierwszą");
+            int liczbaA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wpisz liczbę drugą");
+            int liczbaB = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Twój wynik mnożenia to:");
+
+            Console.WriteLine(liczbaA * liczbaB);
+            Console.ReadKey();
+        }
+    }
+}
